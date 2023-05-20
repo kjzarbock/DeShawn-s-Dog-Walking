987aa2eb181dd2a0e22f86560d852c2b0c58be84
Katharine Zarbock <kjzarbock@gmail.com>
2023-05-20 04:50:19 -0500

    Final

---
+++ scripts/CityList.js
@@ -0,0 +1,27 @@
+import { getWalkers } from "./database.js"
+
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    (clickEvt) => {
+        const cityTarget = clickEvt.target
+        if (cityTarget.dataset.type === "city"){
+            window.alert(`The walker who services this city is ${cityTarget.dataset.walkername}`)
+        }
+        
+    }
+)
+
+export const CityList = () => {
+    let citiesHTML = "<ol>"
+
+    for (const walker of walkers) {
+        citiesHTML += `<li data-type="city" data-walkername="${walker.name}">${walker.city}</li>`
+    }
+
+    citiesHTML += "</ol>"
+
+    return citiesHTML
+}
+
